80f95e6acd6f957dec9b8224468d20d70674d0e7
linh.nguyen <linh.nguyen@BBBW2D3>
2022-08-24 11:31:33 +0700

    duc commit

--- Test Suites/FPT test.ts
+++ Test Suites/FPT test.ts
@@ -12,10 +12,17 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>ea8c7540-9cff-480a-8ec0-41e71670511a</testSuiteGuid>
    <testCaseLink>
-      <guid>29cb4d46-c1fb-42de-b5a8-7cac7d58b837</guid>
+      <guid>bc88a668-7f3d-440d-991e-5d05f892e3e6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>87a68089-a918-4052-80f9-199f7a4ec50d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify Correct Alarm Message</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
